d130eb5150c2b7c71796538486db222cb094082b
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-20 22:21:53 +0530

    Update Simple Example.cs

--- Simple Example.cs
+++ Simple Example.cs
@@ -5,3 +5,33 @@ class Program
             System.Console.WriteLine("Hello World!");  
         }  
     }  
+
+
+using System;  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+            Console.WriteLine("Hello World!");  
+        }  
+    } 
+using System;  
+    public class Program  
+    {  
+        public static void Main(string[] args)  
+        {  
+            Console.WriteLine("Hello World!");  
+        }  
+
+
+using System;  
+namespace ConsoleApplication1  
+{  
+    public class Program  
+    {  
+        public static void Main(string[] args)  
+        {  
+            Console.WriteLine("Hello World!");  
+        }  
+    }  
+}  
