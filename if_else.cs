1d7e8d4f5dfdb30e786c05e61520b418be224a2d
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 01:47:51 +0530

    Create if_else.cs

---
+++ if_else.cs
@@ -0,0 +1,35 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+using System.Threading.Channels;
+namespace ConsoleApp11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = -3;
+            if (a == 3)
+            {
+                Console.WriteLine("equals");
+            }
+            else if (a < -1)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("not equals");
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
+
