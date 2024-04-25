0be00923f6298ab7f42e9fe1f98ce1a87398bb9f
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 01:21:59 +0530

    Create math.cs

---
+++ math.cs
@@ -0,0 +1,31 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading.Channels;
+namespace ConsoleApp11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = -4;
+            Console.WriteLine(Math.Abs(a));
+            Console.WriteLine(Math.Max(10,34));
+            Console.WriteLine(Math.Pow(2,2));
+
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Round(3.432));
+            Console.WriteLine(Math.Log(10));
+            Console.WriteLine(Math.Log10(1));
+            Console.WriteLine(Math.Log(0));
+
+
+
+
+        }
+
+
+
+    }
+}
+
