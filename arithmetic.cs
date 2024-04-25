cc7c8c4af571ffe977a2566491878dfda1aec95c
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-26 01:17:03 +0530

    Create arithmetic.cs

---
+++ arithmetic.cs
@@ -0,0 +1,30 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading.Channels;
+namespace ConsoleApp11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 4;
+            Console.WriteLine(a++);
+            Console.WriteLine(--a);
+            Console.WriteLine(a+=2);
+            Console.WriteLine(a*=2);
+            Console.WriteLine(a/=4);
+            Console.WriteLine(a/=3);
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+
+
+    }
+}
+
